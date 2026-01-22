9ced1e7de11ecb94c987eef73ec92159b12ace9e
Priyansh <Priyansh@Priyanshs-MacBook-Air.local>
2026-01-23 01:17:01 +0530

    api.js fixed

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 5000,
 });
+
 
 // ---------------- REQUEST INTERCEPTOR ----------------
 api.interceptors.request.use(
